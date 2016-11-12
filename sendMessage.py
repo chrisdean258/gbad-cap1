8a52e0458071ec8a8bcb8dac59982a0c6a2ac59c
Chase Nelson <chase.a.nelson@gmail.com>
2016-11-12 10:50:59 -0600

    will send messages to Chase's phone

--- sendMessage.py
+++ sendMessage.py
@@ -6,7 +6,7 @@ AUTH_TOKEN = "0763d7872314d7e5581a8711cb7afda2"
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
-client.message.create(
+client.messages.create(
     to="+13179418233",
     from_="+13178545402",
     body="This is a test message. The quick brown fox jumps over the lazy dog.",
