902c2ad09d8ae16ffdb2250440f030758e587cf8
Chase Nelson <chase.a.nelson@gmail.com>
2016-11-12 12:59:40 -0600

    Reads from fraudFile and sends text message

--- sendMessage.py
+++ sendMessage.py
@@ -4,14 +4,22 @@ from twilio.rest import TwilioRestClient
 ACCOUNT_SID = "AC56fa4aee86c0551cf51f9b36cd4f568d"
 AUTH_TOKEN = "0763d7872314d7e5581a8711cb7afda2"
 
-toPhoneNumber="+13179418233"
-fromPhoneNumber="+13178545402"
-message="This is a test message"
+f = open('fraudFile', 'r')
+info = f.read()
+
+toPhoneNumber = info[0:13]
+message = info[13:]
+
+fromPhoneNumber = "+13178545402"
+
+print(info)
+print(toPhoneNumber)
+print(message)
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 client.messages.create(
-    to=toPhoneNumber,
-    from_=fromPhoneNumber,
-    body=message,
+    to = str(toPhoneNumber),
+    from_ = fromPhoneNumber,
+    body = message,
 )
