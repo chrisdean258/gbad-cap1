57d0fa1b4f2d9055903ac7754f98874982604fe2
Chase Nelson <chase.a.nelson@gmail.com>
2016-11-12 19:35:25 -0600

    Added comments

--- sendMessage.py
+++ sendMessage.py
@@ -4,20 +4,23 @@ from twilio.rest import TwilioRestClient
 ACCOUNT_SID = "AC56fa4aee86c0551cf51f9b36cd4f568d"
 AUTH_TOKEN = "0763d7872314d7e5581a8711cb7afda2"
 
+# open a file named fraudFile.txt in read mode
 f = open('fraudFile.txt', 'r')
+# read the file into a string info
 info = f.read()
 
+# parse the phone number from the info string
 toPhoneNumber = info[0:13]
+# parse the message from the info string
 message = info[13:]
 
+# auto phone number from Twilio to send messages
 fromPhoneNumber = "+13178545402"
 
-print(info)
-print(toPhoneNumber)
-print(message)
-
+# access the API client
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
+# create and send a client message with the two phone nubmers and the message
 client.messages.create(
     to = str(toPhoneNumber),
     from_ = fromPhoneNumber,
